e0428395c88eccb8b73c4b10acb1adefdc8a0457
Dmytro Shumsky <schooldima1@gmail.com>
2024-05-06 16:09:34 +0300

    all pagination without added recipe redirection

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -10,18 +10,22 @@ import {UserContext} from "../contexts.js";
 
 
 const Recipes = () => {
-    const [offset, setOffset] = useState(0);
+    const [offset1, setOffset1] = useState(0);
+    const [offset2, setOffset2] = useState(0);
     const [data, setData] = useState([]);
     const [perPage, setPerPage] = useState(8);
-    const [currentPage, setCurrentPage] = useState(0);
-    const [pageCount, setPageCount] = useState(0);
-    const [postData, setPostData] = useState([]);
+    const [currentPage1, setCurrentPage1] = useState(0);
+    const [currentPage2, setCurrentPage2] = useState(0);
+    const [pageCount1, setPageCount1] = useState(0);
+    const [pageCount2, setPageCount2] = useState(0);
+    const [postData1, setPostData1] = useState([]);
+    const [postData2, setPostData2] = useState([]);
 
     const {user} = useContext(UserContext);
 
     const receivedData = async () => {
         if (user && user.liked_posts) {
-            const data = await Promise.all(user.liked_posts.slice(offset, offset + perPage).map(id =>
+            const data = await Promise.all(user.liked_posts.slice(offset1, offset1 + perPage).map(id =>
                 axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
                     .then(res => res.data.meals[0])
             ));
@@ -32,38 +36,66 @@ const Recipes = () => {
                 </React.Fragment>
             ));
     
-            setPageCount(Math.ceil(user.liked_posts.length / perPage));
-            setPostData(postData);
+            setPageCount1(Math.ceil(user.liked_posts.length / perPage));
+            setPostData1(postData);
         }
     };
 
-    const handlePageClick = (e) => {
+    const receivedData2 = async () => {
+        const response = await axios.get(`http://localhost:3001/recipes`);
+        const allRecipes = response.data;
+    
+        const userRecipes = allRecipes.filter(recipe => recipe.author_id === user.id);
+    
+        const postData = userRecipes.slice(offset2, offset2 + perPage).map(meal => (
+            <React.Fragment>
+                <DishItem key={meal.idMeal} id={meal.idMeal} name={meal.strMeal} thumb={meal.strMealThumb} />
+            </React.Fragment>
+        ));
+    
+        setPageCount2(Math.ceil(userRecipes.length / perPage));
+        setPostData2(postData);
+    };
+
+    const handlePageClick1 = (e) => {
         const selectedPage = e.selected;
         const offset = selectedPage * perPage;
 
-        setCurrentPage(selectedPage);
-        setOffset(offset);
+        setCurrentPage1(selectedPage);
+        setOffset1(offset);
+    };
+
+    const handlePageClick2 = (e) => {
+        const selectedPage = e.selected;
+        const offset = selectedPage * perPage;
+    
+        setCurrentPage2(selectedPage);
+        setOffset2(offset);
     };
 
     useEffect(() => {
         receivedData();
-    }, [offset]);
+    }, [offset1]);
+    
+    useEffect(() => {
+        receivedData2();
+    }, [offset2]);
 
     return (
         <div className='pagination-wrapper'>
             <h1>Liked recipes</h1>
             <div className="wrap-postData">
-                {postData}
+                {postData1}
             </div>
             <ReactPaginate
                 previousLabel={"prev"}
                 nextLabel={"next"}
                 breakLabel={"..."}
                 breakClassName={"break-me"}
-                pageCount={pageCount}
+                pageCount={pageCount1}
                 marginPagesDisplayed={2}
                 pageRangeDisplayed={5}
-                onPageChange={handlePageClick}
+                onPageChange={handlePageClick1}
                 containerClassName={"pagination"}
                 subContainerClassName={"pages pagination"}
                 activeClassName={"active"}
@@ -71,7 +103,7 @@ const Recipes = () => {
             <div id="bottom-pd">
                 <h1>Added recipes</h1>
                 <div className="wrap-postData">
-                    {postData}
+                    {postData2}
                 </div>
             </div>
             <ReactPaginate
@@ -79,10 +111,10 @@ const Recipes = () => {
                 nextLabel={"next"}
                 breakLabel={"..."}
                 breakClassName={"break-me"}
-                pageCount={pageCount}
+                pageCount={pageCount2}
                 marginPagesDisplayed={2}
                 pageRangeDisplayed={5}
-                onPageChange={handlePageClick}
+                onPageChange={handlePageClick2}
                 containerClassName={"pagination"}
                 subContainerClassName={"pages pagination"}
                 activeClassName={"active"}
