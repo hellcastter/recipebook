77976c18c4bdaaba147338ffaeef797392cf214e
Victor Muryn <muryn.pn@ucu.edu.ua>
2024-05-06 22:17:53 +0300

    added click on recipies page

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,21 +1,17 @@
-import React, { useState, useEffect, useContext } from 'react';
+import {useContext, useEffect, useState} from 'react';
 import axios from 'axios';
 import ReactPaginate from 'react-paginate';
 import './Recipes.css';
 import DishItem from "../components/dish_item/DishItem.jsx";
-import { ApiContext } from "../contexts.js";
 
-import useSWR from "swr";
 import {UserContext} from "../contexts.js";
 
 
 const Recipes = () => {
+    const perPage = 8;
+
     const [offset1, setOffset1] = useState(0);
     const [offset2, setOffset2] = useState(0);
-    const [data, setData] = useState([]);
-    const [perPage, setPerPage] = useState(8);
-    const [currentPage1, setCurrentPage1] = useState(0);
-    const [currentPage2, setCurrentPage2] = useState(0);
     const [pageCount1, setPageCount1] = useState(0);
     const [pageCount2, setPageCount2] = useState(0);
     const [postData1, setPostData1] = useState([]);
@@ -29,13 +25,11 @@ const Recipes = () => {
                 axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
                     .then(res => res.data.meals[0])
             ));
-    
-            const postData = data.map(meal => (
-                <React.Fragment>
-                    <DishItem key={meal.idMeal} id={meal.idMeal} name={meal.strMeal} thumb={meal.strMealThumb} />
-                </React.Fragment>
+
+            const postData = data.map((meal) => (
+                <DishItem key={meal.idMeal} id={meal.idMeal} name={meal.strMeal} thumb={meal.strMealThumb}/>
             ));
-    
+
             setPageCount1(Math.ceil(user.liked_posts.length / perPage));
             setPostData1(postData);
         }
@@ -44,15 +38,13 @@ const Recipes = () => {
     const receivedData2 = async () => {
         const response = await axios.get(`http://localhost:3001/recipes`);
         const allRecipes = response.data;
-    
+
         const userRecipes = allRecipes.filter(recipe => recipe.author_id === user.id);
-    
-        const postData = userRecipes.slice(offset2, offset2 + perPage).map(meal => (
-            <React.Fragment>
-                <DishItem key={meal.idMeal} id={meal.idMeal} name={meal.strMeal} thumb={meal.strMealThumb} />
-            </React.Fragment>
+
+        const postData = userRecipes.slice(offset2, offset2 + perPage).map((meal) => (
+            <DishItem key={`own/${meal.id}`} id={`own/${meal.id}`} name={meal.strMeal} thumb={meal.strMealThumb}/>
         ));
-    
+
         setPageCount2(Math.ceil(userRecipes.length / perPage));
         setPostData2(postData);
     };
@@ -61,22 +53,20 @@ const Recipes = () => {
         const selectedPage = e.selected;
         const offset = selectedPage * perPage;
 
-        setCurrentPage1(selectedPage);
         setOffset1(offset);
     };
 
     const handlePageClick2 = (e) => {
         const selectedPage = e.selected;
         const offset = selectedPage * perPage;
-    
-        setCurrentPage2(selectedPage);
+
         setOffset2(offset);
     };
 
     useEffect(() => {
         receivedData();
     }, [offset1]);
-    
+
     useEffect(() => {
         receivedData2();
     }, [offset2]);
